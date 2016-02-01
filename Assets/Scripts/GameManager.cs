9a08c19a8f2a6f1a1db8b708821d3dde12db92cb
Eleftherios Manousakis <lemanousakis@gmail.com>
2016-02-01 15:27:47 +0100

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject Room;
+    public List<SpawnableObject> ObjectsToPlace;
+
+    private List<SpawnableObject> _placedObjects = new List<SpawnableObject>();
+
+    private void Start() {
+        BeginGame();
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            RestartGame();
+        }
+    }
+
+    private void BeginGame() {
+
+        Vector3 RoomSize = Room.GetComponent<Collider>().bounds.size;
+        Vector3 RoomBoundaries;
+        RoomBoundaries.x = (RoomSize.x / 2f) - 1;
+        RoomBoundaries.y = 0;
+        RoomBoundaries.z = (RoomSize.z / 2f) - 1;
+
+        foreach (var obj in ObjectsToPlace) {
+            SpawnableObject tmp = Instantiate(obj) as SpawnableObject;
+            Vector3 tmpBounds = tmp.GetComponent<Collider>().bounds.size;
+
+            Vector3 tmpPosition =
+                new Vector3(Random.Range(-RoomBoundaries.x, RoomBoundaries.x), tmpBounds.y - tmpBounds.y / 2f, Random.Range(-RoomBoundaries.z, RoomBoundaries.z));
+
+            tmp.transform.position = tmpPosition;
+            tmp.transform.rotation = Quaternion.identity;
+            _placedObjects.Add(tmp);
+        }
+    }
+
+    public void RemoveDestroyedObject(SpawnableObject sobj) {
+        _placedObjects.Remove(sobj);
+        Debug.LogWarning("Restarting as an object has been deleted");
+        RestartGame();
+    }
+
+    private void RestartGame() {
+
+        foreach (var obj in _placedObjects) {
+            Destroy(obj.gameObject);
+        }
+
+        _placedObjects.Clear();
+        BeginGame();
+    }
+}
---
+++ Assets/Scripts/SpawnableObject.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class SpawnableObject : MonoBehaviour {
+
+    void OnCollisionEnter(Collision col) {
+        if (col.gameObject.GetComponent<SpawnableObject>() != null) {
+            GameManager tmp = FindObjectOfType<GameManager>();
+            tmp.RemoveDestroyedObject(this);
+            Destroy(gameObject);
+        }
+    }
+}
