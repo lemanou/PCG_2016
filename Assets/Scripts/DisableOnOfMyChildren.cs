623e85850ffb277e222bc90f0d709ef9edbca42e
Eleftherios Manousakis <lemanousakis@gmail.com>
2016-03-02 11:31:35 +0100

    Randomize bookcase ~ done

---
+++ Assets/Scripts/DisableOnOfMyChildren.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class DisableOnOfMyChildren : MonoBehaviour {
+
+    void Start() {
+        List<GameObject> goList = new List<GameObject>();
+        var children = gameObject.GetComponentsInChildren<Transform>();
+        int count = 0;
+        foreach (var child in children) {
+            if (child.parent == gameObject.transform) {
+                count++;
+                goList.Add(child.gameObject);
+                //print(child + ": " + gameObject.name);
+            }
+        }
+
+        int random = Random.Range(-1, goList.Count - 1);
+        if (random > -1)
+            goList[random].gameObject.SetActive(false);
+    }
+}
