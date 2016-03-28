317e1b98696742a69fdf12afb6852cad9a4a8aa8
Sigurd Knarhøi Johannsen <sigurd_johannsen@hotmail.com>
2016-03-28 20:02:36 +0200

    loading screen and redid buttons.

---
+++ Assets/Scripts/LoadingScreenImage.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+//////////////////////////////////////////////////
+//  This script removes the loading screen after 5 seconds. This is made to ensure that the test person sees no difference in "loading time".
+//  The loading screen is also used as background for the menu.
+//////////////////////////////////////////////////
+
+public class LoadingScreenImage : MonoBehaviour
+{
+    public Image _loadingImage;
+    public Text _loadingText;
+
+    private float _timer;
+
+    void Start()
+    {
+        _loadingImage.enabled = true;
+        if (SceneManager.GetActiveScene().name != "SelectionMenu")
+        {
+            _loadingText.enabled = true;
+        }
+        else _loadingText.enabled = false;
+    }
+
+    void Update()
+    {
+        _timer = Time.timeSinceLevelLoad;
+
+        if (SceneManager.GetActiveScene().name != "SelectionMenu")
+        {
+            if (_loadingImage.enabled == true && _timer > 5)
+            {
+                _loadingImage.enabled = false;
+                _loadingText.enabled = false;
+            }
+        }
+    }
+}
